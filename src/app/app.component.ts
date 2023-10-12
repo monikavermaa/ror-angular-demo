cf94df420b60ef4e0a8ae12299849ad620410604
Monika Verma <monika.verma@taskcraft.in>
2023-10-12 13:22:02 +0530

    my first angular..demo..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; // Import HttpHeaders
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'my-angular-app';
+export class AppComponent implements OnInit {
+  title: string = 'My Angular App';
+  tasks: any[] = [];
+  newTask: any = { title: '', description: '' }; // Initialize a new task object
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    // Fetch existing tasks when the component initializes
+    this.fetchTasks();
+  }
+
+  fetchTasks() {
+    // Update the URL to point to your Rails server
+    this.httpClient.get('http://localhost:3000/api/tasks').subscribe((data: any) => {
+      this.tasks = data.task; // Assuming the response structure has a 'task' property
+    });
+  }
+  
+  createTask() {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'X-CSRF-TOKEN': this.getCSRFTokenFromCookie(), // Replace with the actual method to get the CSRF token from the cookie
+    });
+  
+    const options = { headers: headers };
+  
+    this.httpClient
+      .post('http://localhost:3000/api/tasks', this.newTask, options)
+      .subscribe(
+        (data: any) => {
+          // Task created successfully, you can handle this case here.
+          // Maybe clear the form or display a success message.
+          this.newTask = { title: '', description: '' };
+        },
+        (error: any) => {
+          // Error occurred during task creation. Handle the error here.
+          console.error('Error creating task:', error);
+          if (error.status === 422) {
+            // Extract and display validation error messages to the user.
+            const validationErrors = error.error;
+            // Handle validation errors here.
+            // You can display validation error messages to the user.
+          }
+        }
+      );
+  }
+  
+  private getCSRFTokenFromCookie(): string {
+    const cookieValue = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('XSRF-TOKEN='));
+    if (cookieValue) {
+      return cookieValue.split('=')[1];
+    }
+    return '';
+  }
+  
 }
